925a1f31c70086a7fb616d67127f667acb697f21
wyldere <greyson@crowhurst.ws>
2025-03-06 21:13:19 -0500

    is nathan a piece of trash?
    is he? but is he?

---
+++ isthisapieceoftrash.py
@@ -0,0 +1,11 @@
+print("Nathan's Face")
+
+print("This is NOT a piece of trash.")
+print("Wait actually....")
+def thisisapieceoftrash():
+    print("This is DEFINITELY a piece of trash")
+
+
+nathan = True
+if nathan:
+    thisisapieceoftrash()
